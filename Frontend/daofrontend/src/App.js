8c9147c058c4fd73d91d277394b511de94835153
Aditya Tiwari <pubgmlord25@gmail.com>
2023-01-16 15:01:00 +0530

    initial commit

---
+++ Frontend/daofrontend/src/App.js
@@ -0,0 +1,12 @@
+import Appbar from "./Components/Appbar";
+import React from "react";
+import "./index.css";
+const App = () => {
+  return (
+    <div>
+      <Appbar />
+    </div>
+  );
+};
+
+export default App;
