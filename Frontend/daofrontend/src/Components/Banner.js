f2bef3a05e5633e39427d479f47683e33bf3cc31
Aditya Tiwari <pubgmlord25@gmail.com>
2023-01-16 15:02:08 +0530

    initial commit

---
+++ Frontend/daofrontend/src/Components/Banner.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Grid from "@mui/material/Grid"; // Grid version 1
+import Grid2 from "@mui/material/Unstable_Grid2"; // Grid version 2
+
+const Banner = () => {
+  return <div></div>;
+};
+
+export default Banner;
