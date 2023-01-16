ee83952cdb0f2188c8df83b8fd7a7dde0e097e7c
Aditya Tiwari <pubgmlord25@gmail.com>
2023-01-16 16:13:31 +0530

    second commit

--- Frontend/daofrontend/src/App.js
+++ Frontend/daofrontend/src/App.js
@@ -1,10 +1,12 @@
 import Appbar from "./Components/Appbar";
 import React from "react";
 import "./index.css";
+import Banner from "./Components/Banner";
 const App = () => {
   return (
     <div>
       <Appbar />
+      <Banner />
     </div>
   );
 };
--- Frontend/daofrontend/src/Components/Banner.js
+++ Frontend/daofrontend/src/Components/Banner.js
@@ -1,9 +1,47 @@
 import React from "react";
 import Grid from "@mui/material/Grid"; // Grid version 1
 import Grid2 from "@mui/material/Unstable_Grid2"; // Grid version 2
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div className="Banner">
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        <Grid xs={6}>
+          <Typography variant="h3" className="typo">
+            The Society DAOs..
+          </Typography>
+          <Typography variant="subtitle1" className="newtext">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+            repellendus esse consequuntur incidunt ipsam omnis eius, beatae
+            illum aliquid delectus cupiditate dolor amet architecto.
+          </Typography>
+
+          <ButtonGroup
+            disableElevation
+            variant="contained"
+            aria-label="Disabled elevation buttons"
+            className="new-btn"
+          >
+            <Button>Read Docs</Button>
+            <Button>Get started</Button>
+          </ButtonGroup>
+        </Grid>
+        <Grid xs={6}>
+          <img
+            src="https://d33wubrfki0l68.cloudfront.net/eeb1c4eb81864cd215bdcbebb63679534af42773/e7824/static/d17b5ecb3655c50d6540e590a93d65e7/87c97/dao-2.png"
+            className="img-1"
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Banner;
